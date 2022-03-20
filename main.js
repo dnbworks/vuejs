3ae4fc17df996ce3f3748acbce6a1c3733a90b3d
dnbworks <dooyongnsaakobalderama@gmail.com>
2022-03-20 22:48:54 +0800

    conditional rendering

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+           product: 'Socks',
+           image: './assets/images/socks_green.jpg',
+           inStock: false,
+           inventory: 0
+        }
+    }
+});
