f3fb14a8082c06af9e795ac3a401f8fff9128374
dnbworks <dooyongnsaakobalderama@gmail.com>
2022-03-21 22:30:27 +0800

    computed properties

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 const app = Vue.createApp({
     data() {
         return {
+          cart: 0,
            product: 'Socks',
+           brand: 'Chanel',
            image: './assets/images/socks_green.jpg',
            inStock: false,
-           inventory: 0
+           inventory: 0,
+           details: ['50% cotton', '30% wool', '20% polyester'],
+           sizes: ['S', 'M', 'L', 'XL'],
+           variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          ]
         }
-    }
+    },
+    methods: {
+      addToCart(){
+        this.cart += 1;
+      },
+      removeFromCart() {
+        if (this.cart >= 1) {
+            this.cart -= 1
+        }
+      },
+      updateImage(variantImage){
+        this.image = variantImage;
+      }
+    },
+    computed: {
+      title() {
+          return this.brand + ' ' + this.product
+      },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].image
+      }
+  }
 });
